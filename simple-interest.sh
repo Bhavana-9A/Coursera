f313b0aed8310f10b2b37d5cf5a25371ae33d12a
Bhavana-9A <140804084+Bhavana-9A@users.noreply.github.com>
2025-08-05 17:11:00 +0530

    Delete simple-interest.sh

--- simple-interest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Enter principal amount:"
-read principal
-echo "Enter rate of interest:"
-read rate
-echo "Enter time period in years:"
-read time
-interest=$(echo "$principal * $rate * $time / 100" | bc -l)
-echo "Simple Interest: $interest"
